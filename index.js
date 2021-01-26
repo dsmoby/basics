fad8e7c0a4c475dc12ebc89f05325615a2246df1
mdevmone <mdevmone@gmail.com>
2021-01-26 13:04:24 +0100

    minor refactoring

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ import Comment from './src/resolvers/Comment'
 
 const server = new GraphQLServer({
     typeDefs: "./src/schema.graphql",
-    resolvers: {
-        Query,
-        Mutation,
-        User,
-        Post,
-        Comment
-    },
-    context: {
-        db
-    }
+    resolvers: {Query, Mutation, User, Post, Comment},
+    context: { db }
 })
 server.start(() => console.log("The server is up and running"))
